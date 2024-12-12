1e3b03f36d0f30436ad7dd987f25978969b22077
FrancoDagostino <86207676+FrancoDagostino@users.noreply.github.com>
2024-12-12 16:35:43 -0300

    Se agrego boton para nuevo entidad

--- src/feature/entityList/module.tsx
+++ src/feature/entityList/module.tsx
@@ -4,8 +4,9 @@ import { EntityTableListComponent } from "./components/EntityListTable.component
 import { IUiHook } from "../ui/hooks/useUi.hook";
 import { useEntityListHook } from "./hooks/useModule.hook";
 import { EntityViewModalComponent } from "./components/EntityViewModal.component";
-import { Button, Grid, Typography } from "@mui/material";
+import { Button, Grid, Typography, Box } from '@mui/material';
 import AddIcon from '@mui/icons-material/Add';
+import AddCircleOutlinedIcon from '@mui/icons-material/AddCircleOutlined';
 
 interface IEntityListProps {
     entityListStore: IEntityListStore
@@ -21,6 +22,17 @@ export const EntityListModule: FC<IEntityListProps> = (props) => {
 
     return (
         <>
+            <Box sx={{ display: 'flex', width: '100%', justifyContent: 'space-between' }}>
+                <h1>Listado de Entidades</h1>
+                <Button
+                    sx={{ height: 50, width: 300, marginLeft: "20px", marginTop: "15px", gap: 1 }}
+                    variant="contained"
+                    onClick={() => props.onNavigate('/entity-add')}
+                    startIcon={<AddCircleOutlinedIcon />}
+                >
+                    Nueva Entidad
+                </Button>
+            </Box>
             <Grid container alignItems="center" justifyContent="space-between" spacing={2} mb={2} mt={2}>
                 <Grid item>
                     <Typography variant="h4">Listado de Entidades</Typography>
